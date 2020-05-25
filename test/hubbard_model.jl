d2797d2476a2aa69ea3c7aeb6c8be04e4c948737
Xiaoliang Pan <panxl@users.noreply.github.com>
2020-05-25 13:55:34 -0500

    Update hubbard_model.jl

--- test/hubbard_model.jl
+++ test/hubbard_model.jl
@@ -1,7 +1,8 @@
 include("../src/JuQC.jl")
+using LinearAlgebra
 using .JuQC
 
 hm = HubbardModel(5, 1.0, 4.0)
 init_guess = Matrix{Float64}(1.0*I, hm.dim, hm.dim)
 this_scf = SelfConsistentField(hm, 1e-20, 10, init_guess, orbtype="UHF")
-kernel!(this_scf)
+JuQC.kernel!(this_scf)
