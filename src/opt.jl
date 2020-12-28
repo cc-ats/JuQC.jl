a1acb5233cb448471022597034fc0fad32d0ad5a
Junjie Yang <yangjunjie0320@gmail.com>
2020-12-28 16:44:32 -0600

    update

--- src/opt.jl
+++ src/opt.jl
@@ -5,16 +5,18 @@ abstract type OptimizationAlgorithm{T} end
 mutable struct RoothaanOptimizer{T} <: OptimizationAlgorithm{T}
     _the_obj_func::ObjectiveFunction{T}
 
-    _tol   ::T
-    _prev_y::T
-    _this_y::T
-    _grad_norm::T
+    _tol      ::Real
+    _prev_y   ::Real
+    _this_y   ::Real
+    _grad_norm::Real
 end
 
 function next_step!(the_optimizer::RoothaanOptimizer{T}, this_y::T) where {T}
     the_optimizer._prev_y    = the_optimizer._this_y
     the_optimizer._this_y    = this_y
-    the_optimizer._grad_norm = norm(get_grad(the_optimizer._the_obj_func))
+
+    grad_size                = sizeof(the_optimizer._grad_norm)
+    the_optimizer._grad_norm = norm(get_grad(the_optimizer._the_obj_func))/sqrt(grad_size)
     
     is_wolfe1 = abs(_this_y-_prev_y)     < the_optimizer._tol
     is_wolfe2 = the_optimizer._grad_norm < the_optimizer._tol
--- src/scf.jl
+++ src/scf.jl
@@ -8,19 +8,20 @@ end
 
 abstract type SCFSolver{T} <: ObjectiveFunction{T} end 
 mutable struct RestrictedSCFSolver{T} <: SCFSolver{T}
-    _is_converged::Bool
-
     _nelec::Integer
     _nocc ::Integer
     _nao  ::Integer
     _nmo  ::Integer
 
+    _guess_method  ::InitGuessMethod
+    _fock_builder  ::RestrictedFockBuilder{T}
+
+    _is_converged  ::Bool
     _orb_ene       ::Array{T, 1}
     _orb_coeff     ::Array{T, 2}
     _orb_coeff_inv ::Array{T, 2}
     _density_matrix::Array{T, 2}
-    _guess_method  ::InitGuessMethod
-    _fock_builder  ::RestrictedFockBuilder{T}
+    _fock_matrix   ::Array{T, 2} 
 end
 
 function get_nao(the_scf::RestrictedSCFSolver)
@@ -31,22 +32,6 @@ function get_nmo(the_scf::RestrictedSCFSolver)
     return the_scf._nmo::Integer
 end
 
-function get_orb_coeff_value(the_scf::RestrictedRestrictedSCFSolver{T}, orb_coeff::Array{T, 2}, lm::Integer, p::Integer) where {T}
-    return orb_coeff[lm, p]::T
-end
-
-function get_orb_coeff_vector(the_scf::RestrictedRestrictedSCFSolver{T}, orb_coeff::Array{T, 2}, p::Integer) where {T}
-    return orb_coeff[:, p]::Array{T, 1}
-end
-
-function get_orb_coeff_block(the_scf::RestrictedRestrictedSCFSolver{T}, p::UnitRange{Integer}) where {T}
-    return the_scf._orb_coeff[:, p]::Array{T, 2}
-end
-
-function get_orb_ene(the_scf::RestrictedRestrictedSCFSolver{T}, p::Integer) where {T}
-    return the_scf._orb_ene[p]::T
-end
-
 function get_occ_index(the_scf::RestrictedRestrictedSCFSolver)
     return (1:the_scf._nocc)::UnitRange{Integer}
 end
@@ -55,64 +40,60 @@ function get_vir_index(the_scf::RestrictedRestrictedSCFSolver)
     return ((the_scf._nocc+1):(the_scf._nmo))::UnitRange{Integer}
 end
 
+function get_eri_value(the_scf::RestrictedRestrictedSCFSolver, lm::Integer, sgm::Integer, mu::Integer, nu::Integer)
+    return get_value(the_scf._fock_builder._eri, lm, sgm, mu, nu)
+end
+
+function build_orbitals(the_scf::RestrictedRestrictedSCFSolver{T}, fock_matrix::Array{T, 2}) where {T}
+    ovlp_matrix = the_scf._fock_builder._ovlp
+    vals, vecs  = eigen(fock_matrix, ovlp_matrix)
+    return vals, vecs
+end
+
 function build_density_matrix(the_scf::RestrictedRestrictedSCFSolver{T}, orb_coeff::Array{T, 2}) where {T}
-    co = get
+    nao       = get_nao(the_scf)
+    occ_index = get_occ_index(the_scf)
+
+    co        = orb_coeff[:, occ_index]
+    co_t      = transpose(x)
+
+    dm        = Array{T}(undef, nao, nao)
+    mul!(dm, co, co_t, 2.0, false)
+    return dm
 end
 
 function build_fock_matrix(the_scf::RestrictedRestrictedSCFSolver{T}, dm::Array{T, 2}) where {T}
-    nao = get_nao(the_scf)
-    
-    jmat = Array{T, 2}(undef, nao, nao)
+    nao   = get_nao(the_scf)
+    hcore = the_scf._fock_builder.hcore
+    jmat  = zeros(T, nao, nao)
+    kmat  = zeros(T, nao, nao)
+    dm_tot            = get_dm_tot(the_scf,  dm)
+    dm_alpha, dm_beta = get_dm_spin(the_scf, dm)
+
+    for lm in 1:nao
+        for sgm in 1:nao
+            for mu in 1:nao
+                for nu in 1:nao
+                    jmat[lm, sgm] += get_eri_value(the_scf, lm, sgm, mu, nu) * dm_tot(nu, mu)
+                    kmat[lm, sgm] += get_eri_value(the_scf, lm, mu, sgm, nu) * dm_alpha(nu, mu)
+                end
+            end
+        end
+    end
+
+    return hcore + jmat - kmat
 end
 
-struct UnrestrictedRestrictedSCFSolver{T}
-    _nelec::Tuple{Integer,Integer}
-    _nocc ::Tuple{Integer,Integer}
-    _nao  ::Integer
-    _nmo  ::Integer
-
-    _orb_ene       ::Array{T, 2}
-    _orb_coeff     ::Array{T, 3}
-    _orb_coeff_inv ::Array{T, 3}
-    _density_matrix::Array{T, 3}
-
-    _guess_method  ::InitGuessMethod
-    _fock_builder  ::UnrestrictedFockBuilder{T}
+function update!(the_scf::RestrictedRestrictedSCFSolver{T}, is_converged::Bool, e_tot::T, orb_ene::Array{T, 1}, orb_coeff::Array{T, 2}, density_matrix::Array{T, 2}, fock_matrix::Array{T, 2}) where {T}
+    the_scf._is_converged   = is_converged
+    the_scf._e_tot          = e_tot
+    the_scf._orb_ene        = orb_ene
+    the_scf._orb_coeff      = orb_coeff
+    the_scf._density_matrix = density_matrix
+    the_scf._fock_matrix    = fock_matrix
 end
 
-function get_nao(the_scf::UnrestrictedSCFSolver)
-    return the_scf._nao::Integer
-end
-
-function get_nmo(the_scf::UnrestrictedSCFSolver)
-    return the_scf._nmo::Integer
-end
-
-function get_orb_coeff_value(the_scf::UnrestrictedUnrestrictedSCFSolver{T}, s::Integer, lm::Integer, p::Integer) where {T}
-    return the_scf._orb_coeff[s, lm, p]::T
-end
-
-function get_orb_coeff_vector(the_scf::UnrestrictedUnrestrictedSCFSolver{T}, s::Integer, p::Integer) where {T}
-    return the_scf._orb_coeff[s, :, p]::Array{T, 1}
-end
-
-function get_orb_coeff_block(the_scf::UnrestrictedUnrestrictedSCFSolver{T}, s::Integer, p::UnitRange{Integer}) where {T}
-    return the_scf._orb_coeff[s, :, p]::Array{T, 2}
-end
-
-function get_orb_ene(the_scf::UnrestrictedUnrestrictedSCFSolver{T}, s::Integer, p::Integer) where {T}
-    return the_scf._orb_ene[s, p]::T
-end
-
-function get_occ_index(the_scf::UnrestrictedUnrestrictedSCFSolver)
-    return ((1:the_scf._nocc[1]), (1:the_scf._nocc[2]))::Tuple{UnitRange{Integer},UnitRange{Integer}}
-end
-
-function get_vir_index(the_scf::UnrestrictedUnrestrictedSCFSolver)
-    return ((the_scf._nocc[1]+1):(the_scf._nmo), (the_scf._nocc[2]+1):(the_scf._nmo))::Tuple{UnitRange{Integer},UnitRange{Integer}}
-end
-
-function kernel!(the_scf::SCFSolver{T}; max_iter::Integer=100, tol::Number=1e-8)
+function kernel!(the_scf::SCFSolver{T}; max_iter::Integer=100, tol::Number=1e-8) where {T}
     
     the_optimizer::OptimizationAlgorithm{T} = algorithm_selection(the_scf, tol)
     reset!(the_optimizer)
@@ -121,19 +102,17 @@ function kernel!(the_scf::SCFSolver{T}; max_iter::Integer=100, tol::Number=1e-8)
     is_converged::Bool = false
 
     while not(is_converged) && iter < max_iter
-        
-        is_converged   = next_step!(the_optimizer, e_tot)
-        mo_coff        = get_orb_coeff(the_scf)
-        density_matrix = build_density_matrix(the_scf, mo_coff)
-        fock_matrix    = build_fock_matrix(the_scf, density_matrix)
-        e_tot          = total_energy(the_scf, density_matrix, fock_matrix)
+
+        orb_coff, orb_ene = build_orbitals(the_optimizer, the_scf)
+        density_matrix    = build_density_matrix(the_scf, orb_coff)
+        fock_matrix       = build_fock_matrix(the_scf, density_matrix)
+        e_tot             = total_energy(the_scf, density_matrix, fock_matrix)
+
+        is_converged      = next_step!(the_optimizer, e_tot)
         update!(
-            the_scf,
-            is_converged,
-            e_tot,
-            mo_coff,
-            density_matrix,
-            fock_matrix
+            the_scf, is_converged, e_tot,
+            orb_ene,        orb_coff,
+            density_matrix, fock_matrix
         )
         iter += 1
     end
