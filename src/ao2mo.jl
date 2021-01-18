0d2c046eb311ea772d4553fb015eb6b3af6ce165
Junjie Yang <yangjunjie0320@gmail.com>
2021-01-17 23:38:06 -0600

    update

--- src/ao2mo.jl
+++ src/ao2mo.jl
@@ -83,3 +83,48 @@ function build_eri_mo(the_scf::RestrictedSCFResult{T,RealType}) where {T,RealTyp
 
     return TwoElectronIntegralMO{T}(eri_mo_spin, f_spin)
 end
+
+function get_mp2_ecorr(the_scf::RestrictedSCFResult{T,RealType}) where {T,RealType}
+    eri_mo = build_eri_mo(the_scf)
+
+    occ = get_occ_index(the_scf)
+    vir = get_vir_index(the_scf)
+
+    e_corr      = zero(RealType)
+    eri_mo_spin = eri_mo._eri_mo_spin
+    f_mo_spin   = eri_mo._f_mo_spin
+
+    for i in occ
+        for j in occ
+            for a in vir
+                for b in vir
+                    ia = 2*(i-1)+1
+                    ib = 2*(i-1)+2
+
+                    ja = 2*(j-1)+1
+                    jb = 2*(j-1)+2
+
+                    aa = 2*(a-1)+1
+                    ab = 2*(a-1)+2
+
+                    ba = 2*(b-1)+1
+                    bb = 2*(b-1)+2
+
+                    e_corr += 0.25*(eri_mo_spin[ia,ja,aa,ba] * eri_mo_spin[ia,ja,aa,ba])/(f_mo_spin[ia] + f_mo_spin[ja] - f_mo_spin[aa] - f_mo_spin[ba])
+
+                    e_corr += 0.25*(eri_mo_spin[ia,jb,ab,ba] * eri_mo_spin[ia,jb,ab,ba])/(f_mo_spin[ia] + f_mo_spin[jb] - f_mo_spin[ab] - f_mo_spin[ba])
+
+                    e_corr += 0.25*(eri_mo_spin[ib,ja,aa,bb] * eri_mo_spin[ib,ja,aa,bb])/(f_mo_spin[ib] + f_mo_spin[ja] - f_mo_spin[aa] - f_mo_spin[bb])
+
+                    e_corr += 0.25*(eri_mo_spin[ia,jb,aa,bb] * eri_mo_spin[ia,jb,aa,bb])/(f_mo_spin[ia] + f_mo_spin[jb] - f_mo_spin[aa] - f_mo_spin[bb])
+
+                    e_corr += 0.25*(eri_mo_spin[ib,ja,ab,ba] * eri_mo_spin[ib,ja,ab,ba])/(f_mo_spin[ib] + f_mo_spin[ja] - f_mo_spin[ab] - f_mo_spin[ba])
+
+                    e_corr += 0.25*(eri_mo_spin[ib,jb,ab,bb] * eri_mo_spin[ib,jb,ab,bb])/(f_mo_spin[ib] + f_mo_spin[jb] - f_mo_spin[ab] - f_mo_spin[bb])
+                    
+                end
+            end
+        end
+    end
+    return e_corr::RealType
+end
